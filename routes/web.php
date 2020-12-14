a53d8432c0dc6fdb13c0d5fb758caa8845b12678
zimzim62000 <zimzim62000@gmail.com>
2020-12-14 18:36:13 +0100

    fix routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JeuController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
