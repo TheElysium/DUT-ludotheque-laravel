ace61307971a68127bccaa172ecce3c41af79955
Herbet Thibaut <thibaut_herbet@ens.univ-artois.fr>
2020-12-16 22:44:39 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use App\Http\Controllers\CommentaireController;
 use App\Http\Controllers\JeuController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
