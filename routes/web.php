bee16e982a406149976f276e259e7d8cbcb6fcc1
Theo FOURNIEZ <theofrnpro@gmail.com>
2020-12-17 09:07:06 +0100

    feat(WIP) : ajout des routes

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,8 @@ Route::get('/jeux/regles/{id}', [JeuController::class, 'regles'])->name('regles'
 
 Route::get('/{rand?}',[HomeController::class,'welcome'])->name('welcome');
 
+Route::get('/profil/create', [UserController::class, 'create'])->name('user.ajoutJeux');
+Route::post('/profil/create', [UserController::class, 'store'])->name('user.storeJeux');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
