aabeb6ddb52d6afcf49fbfae52f960ca7c820336
Nathanaël Bertrand <nathanael_bertrand@ens.univ-artois.fr>
2020-12-17 10:28:11 +0100

    feat: ajout des système des filtrage (issues 16, 17, 18)

--- resources/views/jeux/index.blade.php
+++ resources/views/jeux/index.blade.php
@@ -10,9 +10,9 @@
         <div class="flex justify-end">
             <a href="{{route('jeux.create')}}"><button class=" bg-blue-600 text-gray-200 px-2 py-2 rounded-md ">Ajouter un jeu</button></a>
             <a href="{{ URL::route('jeux.index', $sort) }}">Trier par nom @if ($filter !== null)<i class="fas  @if ($sort == 0)fa-sort-down @else fa-sort-up @endif "></i> @endif</a>
-            <select name="Filtrer par thème..." id="filter_theme" onchange="filtreTheme.call(this.options[this.selectedIndex].text)"></select>
-            <select name="Filtrer par éditeur..." id="filter_editeur" onchange="filtreEditeurs.call(this.options[this.selectedIndex].text)"></select>
-            <select name="Filtrer par mécanique..." id="filter_mecanique" onchange="filtreMecaniques.call(this.options[this.selectedIndex].text)"></select>
+            <span style="margin-left: 5%;"><label for="filter_theme">Filtrer par thème...</label><select name="filter_theme" id="filter_theme" onchange="filtre.call('theme', this)"></select></span>
+            <span style="margin-left: 5%;"><label for="filter_editeur">Filtrer par éditeur...</label><select name="filter_editeur" id="filter_editeur" onchange="filtre.call('editeur', this)"></select></span>
+            <span style="margin-left: 5%;"><label for="filter_mecanique">Filtrer par mécanique...</label><select name="filter_mecanique" id="filter_mecanique" onchange="filtre.call('tag', this)"></select></span>
         </div>
         <h1>Liste des jeux</h1>
         <table class="table-auto">
@@ -43,17 +43,17 @@
 
             for (let c = 0; c < cartes.length; c++) {
                 const carte = cartes.item(c);
-                let theme = carte.getElementsByClassName("theme")[0].innerHTML;
+                let theme = carte.getElementsByClassName("theme")[0].innerText;
                 if (!(filter_theme_array.includes(theme))) {
                     filter_theme_array.push(theme);
                 }
-                let editeur = carte.getElementsByClassName("editeur")[0].innerHTML;
+                let editeur = carte.getElementsByClassName("editeur")[0].innerText;
                 if (!(filter_editeur_array.includes(editeur))) {
                     filter_editeur_array.push(editeur);
                 }
                 let mecaniques = carte.getElementsByClassName("tag");
                 for (let m = 0; m < mecaniques.length; m++) {
-                    const mecanique = mecaniques.item(m).innerHTML;
+                    const mecanique = mecaniques.item(m).innerText;
                     if (!(filter_mecanique_array.includes(mecanique))) {
                         filter_mecanique_array.push(mecanique);
                     }
@@ -79,42 +79,15 @@
             }
         }
 
-        function filtreMecaniques(...mecaniques) {
+        function filtre(option) {
             var cartes = document.getElementsByClassName("carte");
+            var classe = {"filter_theme" : "theme", "filter_editeur" : "editeur", "filter_mecanique" : "tag"}[option.id];
             for (let carte of cartes) {
                 carte.style.display = "block";
                 var found = false;
-                for (let tag of carte.getElementsByClassName("tag")) {
-                    alert(tag.innerHTML)
-                    if (mecaniques.includes(tag.innerHTML)) {
-                        found = true; break;
-                    }
-                }
-                if (!found) carte.style.display = "none";
-            }
-        }
-
-        function filtreEditeurs(...editeurs) {
-            var cartes = document.getElementsByClassName("carte");
-            for (let carte of cartes) {
-                carte.style.display = "block";
-                var found = false;
-                for (let editeur of carte.getElementsByClassName("editeur")) {
-                    if (editeurs.includes(editeur.innerHTML)) {
-                        found = true; break;
-                    }
-                }
-                if (!found) carte.style.display = "none";
-            }
-        }
-
-        function filtreTheme(...themes) {
-            var cartes = document.getElementsByClassName("carte");
-            for (let carte of cartes) {
-                carte.style.display = "block";
-                var found = false;
-                for (let theme of carte.getElementsByClassName("theme")) {
-                    if (themes.includes(theme.innerHTML)) {
+                for (let e of carte.getElementsByClassName(classe)) {
+                    // if (option === e.innerText) {
+                    if (option.options[option.selectedIndex].innerText === e.innerText) {
                         found = true; break;
                     }
                 }
