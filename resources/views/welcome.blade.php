4f36d751bbbaa6c29e4e977d1ffed9f086bdbdfc
Nathanaël Bertrand <nathanael_bertrand@ens.univ-artois.fr>
2020-12-17 16:38:59 +0100

    fix, wip: Front de la page d'accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,34 @@
 @extends('base.master')
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-@auth
-	<a href="{{ route('dashboard') }}" class="no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Dashboard</a>
-    <a href="{{ route('welcome', ['options' => 'rand']) }}">5 Jeux aléatoires</a>
-    <a href="{{ route('welcome', ['options' => 'best']) }}">5 Meilleurs jeux</a>
-	<a href="{{ url('profil') }}" class="no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Profil</a>
-@else
-    <a href="{{ route('login') }}" class="no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Connexion</a>
-	@if (Route::has('register'))
-		<a href="{{ route('register') }}" class="no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Enregistrement</a>
-	@endif
-@endauth
-<a href="{{ route('jeux.index') }}" class="no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Jeux</a>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Ludo Rancho</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="nav-item"><a href="{{ route('jeux.index') }}" class="nav-link no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Jeux</a></li>
+        @auth
+                <li class="nav-item"><a href="{{ route('welcome', ['options' => 'rand']) }}" class="nav-link">5 Jeux aléatoires</a></li>
+                <li class="nav-item"><a href="{{ route('welcome', ['options' => 'best']) }}" class="nav-link">5 Meilleurs jeux</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Dashboard</a></li>
+                <li class="nav-item"><a href="{{ url('profil') }}" class="nav-link no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Profil</a></li>
+            </ul>
+        @else
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                @if (Route::has('register'))
+                    <li class="nav-item"><a href="{{ route('register') }}" class="no-underline hover:text-gray-200 hover:text-underline py-2 px-4">
+                        <span class="glyphicon glyphicon-user"></span>Enregistrement
+                    </a></li>
+                @endif
+                <li class="nav-item"><a href="{{ route('login') }}" class="no-underline hover:text-gray-200 hover:text-underline py-2 px-4">
+                    <span class="glyphicon glyphicon-log-in"></span> Connexion
+                </a></li>
+            </ul>
+        @endauth
+    </div>
 </nav>
 
 @yield('aleatoire')
